ed010eea25d52075a555129c7ab3d0852380fb52
Alex Ling <alex@upwardautomation.com>
2015-02-27 21:54:28 +0000

    Resolved merge on resource.js

--- PowerTower/www/src/resource.js
+++ PowerTower/www/src/resource.js
@@ -1,13 +1,10 @@
 var asset = {
     HelloWorld_png : "asset/HelloWorld.png",
     CloseNormal_png : "asset/CloseNormal.png",
-<<<<<<< HEAD
+
     CloseSelected_png : "asset/CloseSelected.png"//,
     //map_01 : "asset/map_01.tmx"
-=======
-    CloseSelected_png : "asset/CloseSelected.png",
-    map_01 : "asset/Map_01.tmx"
->>>>>>> 3468397eda5c564e1763a4e4e48bb6198dc82601
+
 };
 
 var g_resources = [];
