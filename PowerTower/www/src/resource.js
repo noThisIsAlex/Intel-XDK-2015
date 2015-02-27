ac8f92dada77da5e77b776ec75597997b7f9bb55
Alex Ling <alex@upwardautomation.com>
2015-02-27 22:18:40 +0000

    Working, without proper map tile.

--- PowerTower/www/src/resource.js
+++ PowerTower/www/src/resource.js
@@ -2,7 +2,7 @@ var asset = {
     HelloWorld_png : "asset/HelloWorld.png",
     CloseNormal_png : "asset/CloseNormal.png",
 
-    CloseSelected_png : "asset/CloseSelected.png"//,
+    CloseSelected_png : "asset/CloseSelected.png",
     map_01 : "asset/tiled_sprites/Map_01.tmx"
 };
 
