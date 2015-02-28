82fd47dcb6e370e3bc6002156da1f3db05ad8e28
digitalstan <codykitchener963@gmail.com>
2015-02-28 11:11:06 -0800

    updated power plant pic and modified resource.js

--- PowerTower/www/src/resource.js
+++ PowerTower/www/src/resource.js
@@ -5,9 +5,10 @@ var asset = {
     map_01 : "asset/tiled_sprites/Map_test01.tmx",
     terrian_tileset : "asset/tiled_sprites/terrain_tiles.png",
     enemy : "asset/tiled_sprites/enmy.png",
-    powerplantlv2: "asset/tiled_sprites/Power Plant.png",
-    powerplantlv1: "asset/tiled_sprites/Power Plant lvl 1.png",
-    powerplantlv3: "asset/tiled_sprites/Power Plant lvl 3.png",
+    powerplant: "asset/tiled_sprites/power_plant.png",
+    powerplantlv1: "asset/tiled_sprites/power_plant_lv1.png",
+    powerplantlv2: "asset/tiled_sprites/power_plant_lv2.png",
+    powerplantlv3: "asset/tiled_sprites/power_plant_lv3.png",
     tower: "asset/tiled_sprites/Basic_Tower_On.png"
 };
 
