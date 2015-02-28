f1f48edc471ad765640f4efa14afad349a42796f
unknown <mikebolt@gmail.com>
2015-02-27 21:42:17 -0800

    added some files

--- PowerTower/www/src/HealthBar.js
+++ PowerTower/www/src/HealthBar.js
@@ -5,10 +5,19 @@ var HealthBar = cc.Layer.extend({
     blackBar: null,
     greenBar: null,
     redBar: null,
-    ctor: function() {
+    ctor: function(currentHealth, totalHealth) {
         this._super();
         blackBar = new cc.LayerColor(cc.color(0, 0, 0, 255), HealthBar_width, HealthBar_height);
         greenBar = new cc.LayerColor(cc.color(0, 200, 0, 255), HealthBar_width - 2, HealthBar_height - 2);
-        // TODO finish writing this
+        redBar = new cc.LayerColor(cc.color(255, 0, 0, 255), HealthBar_width - 2, HealthBar_height - 2);
+        
+        greenBar.x = 1;
+        greenBar.y = 1;
+        redBar.x = 1;
+        redBar.y = 1;
+        
+        this.addChild(blackBar, 1);
+        this.addChild(redBar, 2);
+        this.addChild(greenBar, 3);
     }
 });
--- PowerTower/www/src/resource.js
+++ PowerTower/www/src/resource.js
@@ -1,8 +1,8 @@
 var asset = {
     HelloWorld_png : "asset/HelloWorld.png",
     CloseNormal_png : "asset/CloseNormal.png",
-    CloseSelected_png : "asset/CloseSelected.png",
-    map_01 : "asset/map_01.tmx"
+    CloseSelected_png : "asset/CloseSelected.png"//,
+    //map_01 : "asset/map_01.tmx"
 };
 
 var g_resources = [];
