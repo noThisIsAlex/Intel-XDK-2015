c003de59086ad34bfbd6c57bee4b86ee42ec0288
digitalstan <codykitchener963@gmail.com>
2015-02-28 13:58:03 -0800

    made a better tileset and a new map

--- PowerTower/www/src/resource.js
+++ PowerTower/www/src/resource.js
@@ -3,7 +3,9 @@ var asset = {
     CloseNormal_png : "asset/CloseNormal.png",
     CloseSelected_png : "asset/CloseSelected.png",
     map_01 : "asset/tiled_sprites/Map_test01.tmx",
+    map_02 : "asset/tiled_sprites/Map_02.tmx",
     terrian_tileset : "asset/tiled_sprites/terrain_tiles.png",
+    terrain_corners : "asset/tiled_sprites/terrain_corners.png",
     enemy : "asset/tiled_sprites/soldier.png",
     powerplant_base : "asset/tiled_sprites/power_plant.png",
     powerplantlv1 : "asset/tiled_sprites/power_plant_lv1.png",
@@ -13,7 +15,8 @@ var asset = {
     tower_down : "asset/tiled_sprites/Basic_Tower_On_p2.png",
     tower_left : "asset/tiled_sprites/Basic_Tower_On_p3.png",
     tower_right : "asset/tiled_sprites/Basic_Tower_On_p4.png",
-    fireball : "asset/tiled_sprites/fireball.png"
+    fireball : "asset/tiled_sprites/fireball.png",
+    tank_decoration : "asset/tiled_sprites/tank_decoration.png"
 };
 
 var g_resources = [];
