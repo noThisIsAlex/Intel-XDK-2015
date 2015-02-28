35ded7548f7cb78c31e6b77ec3e894338f67bba6
unknown <mikebolt@gmail.com>
2015-02-27 23:13:45 -0800

    fixed main.js

--- PowerTower/www/main.js
+++ PowerTower/www/main.js
@@ -64,8 +64,7 @@ cc.game.onStart = function(){
      * [5] FIXED_WIDTH     Scale the content's width to screen's width and proportionally scale its height
      * See [official documentation](https://github.com/chukong/cocos-docs/blob/master/manual/framework/html5/v2/resolution-policy-design/en.md) for details
      */
-    var ws = cc.winSize
-    cc.view.setDesignResolutionSize(ws.width, ws.height, cc.ResolutionPolicy.NO_BORDER);
+    cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
     /**
      * Sets whether resize canvas automatically when browser's size changed.
      */
